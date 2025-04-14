b2a9f98bc086e297e4d5ab4be7a03a40598b0181
Joao Felipe Rodrigues-FELIPERL <feliperl@motorola.com>
2025-04-14 19:21:56 -0400

    versão final

--- src/components/Information.js
+++ src/components/Information.js
@@ -11,7 +11,7 @@ function Information() {
                 <h3>Traje</h3>
                 <p>
                     Pensando na harmonia do nosso grande dia, reservamos algumas cores para os padrinhos e o noivo.
-                    Contamos com seu carinho em escolher cores alternativas aos <strong>tons de azul e cinza</strong>,
+                    Contamos com seu carinho em escolher cores alternativas aos <strong>tons de azul</strong>,
                     e pedimos que venha vestido em traje <strong>ESPORTE FINO</strong>.
                 </p>
 
