1afb9dd0c58521e9102f79a2fa231794c081fda8
Joao Felipe Rodrigues-FELIPERL <feliperl@motorola.com>
2025-04-22 10:34:15 -0400

    ajustes na informação

--- src/components/Information.js
+++ src/components/Information.js
@@ -10,11 +10,8 @@ function Information() {
             <div id="info" className="information-traje">
                 <h3>Traje</h3>
                 <p>
-                    Pensando na harmonia do nosso grande dia, reservamos algumas cores para os padrinhos e o noivo.
-                    Contamos com seu carinho em escolher cores alternativas aos <strong>tons de azul</strong>,
-                    e pedimos que venha vestido em traje <strong>ESPORTE FINO</strong>.
+                    Para manter a harmonia do nosso grande dia, reservamos algumas cores especiais para os padrinhos e o noivo. Pedimos, com todo carinho, que evitem o uso de <strong>tons de azul</strong> e escolham opções alternativas. Além disso, solicitamos que venham vestidos com traje <strong>ESPORTE FINO</strong>.
                 </p>
-
             </div>
         </section>
     );
